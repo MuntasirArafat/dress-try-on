26a15aae7bdd96299089269a3424b34bf00159b9
Muntasir Arafat <103080138+MuntasirArafat@users.noreply.github.com>
2026-06-10 23:52:05 +0600

    Increase timeout for image download to 120 seconds

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 
 
 def download_image(url, path):
-    response = requests.get(url, timeout=60)
+    response = requests.get(url, timeout=120)
     response.raise_for_status()
 
     with open(path, "wb") as f:
